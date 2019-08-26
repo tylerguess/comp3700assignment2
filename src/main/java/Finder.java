24fe28c7863024386a5feb7a4b5534d9c8c8a398
Tyler Guess <tylerguess@Tylers-MacBook-Pro.local>
2019-08-26 12:37:55 -0500

    Initial commit

---
+++ src/main/java/Finder.java
@@ -0,0 +1,34 @@
+public class Finder {
+
+    public java.lang.Integer findMin(int[] inputArray) {
+
+        if (inputArray == null) {
+            return null;
+        } else if (inputArray.length <= 0) {
+            return null;
+        }
+        int min = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++) {
+            if (inputArray[i] < min) {
+                min = inputArray[i];
+            }
+        }
+        return min;
+    }
+
+    public java.lang.Integer findMax(int[] inputArray) {
+
+        if (inputArray == null) {
+            return null;
+        } else if (inputArray.length <= 0) {
+            return null;
+        }
+        int max = inputArray[0];
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] > max) {
+                max = inputArray[i];
+            }
+        }
+        return 0;
+    }
+}
